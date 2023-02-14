39f148dce2dd396b4428b334d629ab5d40e36653
ericburt1 <112107869+ericburt1@users.noreply.github.com>
2023-02-14 13:38:02 -0700

    this is a redo to make the running work.
    Open vscode and open the chatgames folder then use this command in the terminal 'cd ChatGPT_games' this will bring you to the correct area to run the program. Then to run the program type 'python3 menu.py'

---
+++ chatgame/ChatGPT_games/menu.py
@@ -0,0 +1,35 @@
+#
+
+
+def start():
+	print("Menu:")
+	print("1. Tic Tac Toe")
+	print("2. Password Manager")
+	print("3. Text Adventure: Escape the Forest")
+
+	selected_game = int(input("Please select a game to play (1, 2, or 3): "))
+
+	if selected_game == 1:
+		from chatgpt_Coding import display_board, main_game
+		print("You have selected Tic Tac Toe.")
+		print("\n" * 100)
+		display_board()
+		main_game()
+
+
+	elif selected_game == 2:
+		# code for Password Manager game
+		print("You have selected Password Manager.")
+		# code for creating, removing, and editing usernames and passwords
+		print("Username and password functionality enabled.")
+
+	elif selected_game == 3:
+		from escapegame import display_title_screen
+		# code for Text Adventure: Escape the Forest game
+		print("You have selected Text Adventure: Escape the Forest.")
+		display_title_screen()
+	else:
+		print("Invalid selection. Please try again.")
+
+
+start()
---
+++ chatgame/README.md
@@ -0,0 +1,4 @@
+# CHATGPT_GAMES
+ChatGPT creates games using python.
+These games are specifically made by chatgpt and should not be used as work that "you" created.
+Currently using this in VScode. To start program type in the termainal "python3 main.py"
